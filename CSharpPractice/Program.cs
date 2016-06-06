fd5d9c8edd2045ac5e3e91d8b94218530f61f2e2
jfeng <cellphone2008@gmail.com>
2016-06-06 17:31:49 -0400

    git bug

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			FirstMissingPositiveSol test = new FirstMissingPositiveSol ();
+			int[] data = new int[]{1, 2};
+			Console.WriteLine (test.FirstMissingPositive(data));
+		}
+	}
+}
